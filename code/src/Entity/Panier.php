45adb30d3f2e0592a027c7efa19332ad8ed9e7b0
404Monkey <60445154+404Monkey@users.noreply.github.com>
2021-03-30 10:59:41 +0200

    Add Entity and database panier

---
+++ code/src/Entity/Panier.php
@@ -0,0 +1,77 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\PanierRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass=PanierRepository::class)
+ */
+class Panier
+{
+    /**
+     * @ORM\Id
+     * @ORM\GeneratedValue
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\ManyToOne(targetEntity=Utilisateur::class)
+     * @ORM\JoinColumn(nullable=false)
+     */
+    private $utilisateur;
+
+    /**
+     * @ORM\ManyToOne(targetEntity=Produit::class)
+     * @ORM\JoinColumn(nullable=false)
+     */
+    private $produit;
+
+    /**
+     * @ORM\Column(type="integer")
+     */
+    private $nb_achete;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getUtilisateur(): ?Utilisateur
+    {
+        return $this->utilisateur;
+    }
+
+    public function setUtilisateur(?Utilisateur $utilisateur): self
+    {
+        $this->utilisateur = $utilisateur;
+
+        return $this;
+    }
+
+    public function getProduit(): ?Produit
+    {
+        return $this->produit;
+    }
+
+    public function setProduit(?Produit $produit): self
+    {
+        $this->produit = $produit;
+
+        return $this;
+    }
+
+    public function getNbAchete(): ?int
+    {
+        return $this->nb_achete;
+    }
+
+    public function setNbAchete(int $nb_achete): self
+    {
+        $this->nb_achete = $nb_achete;
+
+        return $this;
+    }
+}
---
+++ main_panier.sql
@@ -0,0 +1,2 @@
+INSERT INTO panier (id, utilisateur_id, produit_id, nb_achete) VALUES (0, 2, 1, 20);
+INSERT INTO panier (id, utilisateur_id, produit_id, nb_achete) VALUES (1, 2, 2, 30);
