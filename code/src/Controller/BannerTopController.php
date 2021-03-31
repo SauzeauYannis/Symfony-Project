6399c6c79a795f142fefe7b3cb577b8da65f8adc
Yannis <sauzeauyannis@gmail.com>
2021-03-31 09:55:44 +0200

    Add banner

--- code/src/Controller/BannerTopController.php
+++ code/src/Controller/BannerTopController.php
@@ -6,28 +6,20 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
-class MenuController extends AbstractController
+class BannerTopController extends AbstractController
 {
     /**
      * @return Response
      */
-    public function menuAction(): Response
+    public function bannerAction(): Response
     {
         $utilisateurId = $this->getParameter('id');
         $em = $this->getDoctrine()->getManager();
         $utilisateurRepository = $em->getRepository('App:Utilisateur');
-        $produitRepository = $em->getRepository('App:Produit');
 
         $utilisateur = $utilisateurRepository->find($utilisateurId);
 
-        $produits = $produitRepository->findAll();
-
-        $args = [
-            'utilisateur' => $utilisateur,
-            'nombre_de_produits' => count($produits)
-        ];
-        return $this->render('layout/menu.html.twig', $args);
+        $args = ['utilisateur' => $utilisateur];
+        return $this->render('layout/banner_top.html.twig', $args);
     }
 }
-
-/* Créé par Yannis Sauzeau et Benjamin Chevais */
