97dcb8fc462b56bacba278a84bbb9efafb7276e3
404Monkey <60445154+404Monkey@users.noreply.github.com>
2021-04-05 20:59:46 +0200

    add a product form

---
+++ code/src/Form/ProduitType.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Produit;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\IntegerType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class ProduitType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('libelle', TextType::class,
+            ['label' => 'libelle'])
+            ->add('prix_unitaire', IntegerType::class,
+            ['label' => 'prix unitaire'])
+            ->add('quantite', IntegerType::class,
+            ['label' => 'quantite'])
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults([
+            'data_class' => Produit::class,
+        ]);
+    }
+}
